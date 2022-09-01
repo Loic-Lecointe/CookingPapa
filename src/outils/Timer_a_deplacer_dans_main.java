4535a8f32e0d946853fe451b0f1f97346bb4e65f
unknown <etienne.pagelegea.etu@univ-lille.fr>
2022-09-01 17:04:45 +0200

    timer

---
+++ src/outils/Timer_a_deplacer_dans_main.java
@@ -0,0 +1,23 @@
+package src.outils;
+
+import java.time.LocalDateTime;
+
+public class Timer_a_deplacer_dans_main {
+	//Ceci n'est pas une vraie classe, on y stocke juste les lignes de code pour le timer
+	//Elles devront être placées dans le main une fois qu'il sera codé
+	//les variables créées (debut et finDuJeu) devront être passées en paramètre à la classe de Calcul de score
+	
+	public void time(){
+		
+		//Placer juste avant le début du jeu
+		LocalDateTime debutDuJeu = LocalDateTime.now();
+		
+		
+		
+		//Placer juste après la fin du jeu
+		LocalDateTime finDuJeu = LocalDateTime.now();
+		
+		
+		
+	}
+}
