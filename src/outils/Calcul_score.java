2c07c1523288fc6fed40529cf16566cf385d4df5
loicl <loicl@LAPTOP-J07OFEOH>
2022-09-01 17:00:29 +0200

    Calcul du score du joueur 2

--- src/outils/Calcul_score.java
+++ src/outils/Calcul_score.java
@@ -1,13 +1,16 @@
 package outils;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Calcul_score {
-	private int time_start;
-	private int time_end;
+	private LocalDateTime time_start;
+	private LocalDateTime time_end;
 	private int score;
 	private int plats_reussi;
 	private int plats_echoue;
 	
-	public Calcul_score(int time_start, int time_end, int score, int plats_reussi, int plats_echoue) {
+	public Calcul_score(LocalDateTime time_start, LocalDateTime time_end, int score, int plats_reussi, int plats_echoue) {
 		this.time_start = time_start;
 		this.time_end = time_end;
 		this.score = score;
@@ -15,24 +18,15 @@ public class Calcul_score {
 		this.plats_echoue = plats_echoue;
 	}
 	
-	public int calcul_temps_joue(int time_start, int time_end) {
-		return time_start - time_end;
+	public long calcul_temps_joue(LocalDateTime time_start, LocalDateTime time_end) {
+		long temps_de_jeu = time_start.until(time_end, ChronoUnit.SECONDS);
+		return temps_de_jeu;
 	}
-	
-	public int calcul_plats_reussi() {
-		int r = 
-	}
-	
-	public int calcul_plats_echoue() {
-		int e = 
-	}
-	
 	
 	public int getScore() {
 		return score;
 	}
 
 	public void calcul_score_final() {
-		
 	}
 }
