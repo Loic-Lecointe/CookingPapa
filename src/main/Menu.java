322749e7809227bc3dcf4616eb4ca54dd2e101b6
nathan.hallez.etu <nathan.hallez.etu@univ-lille.fr>
2022-09-02 15:39:15 +0200

    Suppression AfficherLogo (pas utilisé)

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -13,35 +13,8 @@ import java.util.Scanner;
 import outils.TableauScores;
 
 public class Menu {
-		static Scanner sc = new Scanner(System.in);
-		static int choix = 0;
-		
-		public static void AfficherLogo() {
-		String path = System.getProperty("user.dir")+"/src/main/";
-		try (FileInputStream fstream =new FileInputStream(path+"logo.txt");BufferedReader br = new BufferedReader(new InputStreamReader(fstream));){
-			String strLine;
-
-			  //Read File Line By Line
-			  while ((strLine = br.readLine()) != null)   {
-			    // Print the content on the console - do what you want to do
-			    System.out.println (strLine);
-			  }
-
-			  //Close the input stream
-			  fstream.close();			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.exit(0);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			System.exit(0);
-
-		}
-		
-		  
-	  }
+	static Scanner sc = new Scanner(System.in);
+	static int choix = 0; 
 	  
 	public static void start() {
 		Main.clearScreen();
